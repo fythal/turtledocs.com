30d8ffe3917ce2d3dadee46bbcdeec2f3b903166
rstewart8 <randystewart8@gmail.com>
2013-09-29 11:18:21 -0600

    migrate changes

--- db/migrate/20130623002756_add_docs.rb
+++ db/migrate/20130623002756_add_docs.rb
@@ -1,11 +1,9 @@
 class AddDocs < ActiveRecord::Migration
   def up
-  	add_column	:documents,	:content_type,	:string
   	add_column	:documents,	:file_size,		:integer
   end
 
   def down
-  	remove_column	:documents,	:content_type,	:string
   	remove_column	:documents,	:file_size,		:integer
   end
 end
